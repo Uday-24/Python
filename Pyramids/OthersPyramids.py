6f79c427409440029abb7c5777330badda4b0c0f
Uday-24 <chotaliyauday24@gmail.com>
2024-06-03 06:39:35 +0530

    New pyramids added

--- Pyramids/OthersPyramids.py
+++ Pyramids/OthersPyramids.py
@@ -40,3 +40,46 @@ def loop1to4():
         print()
 
 loop1to4()
+
+# 5 Triangle        #6 Inverted Triangle      #7 Half Diamond        #8 Flipped Half Diamond 
+#     *                  * * * * *                  *                             *
+#    * *                  * * * *                   * *                         * *
+#   * * *                  * * *                    * * *                     * * *
+#  * * * *                  * *                     * *                         * *
+# * * * * *                  *                      *                             *
+
+def loop5to8():
+    print()
+    print("5: Triangle")
+    size = 5
+    for i in range(1, size+1):
+        for j in range(i, size):
+            print(end=" ")
+        for j in range(1, i+1):
+            print("*", end=" ")
+        print()
+
+    print()
+    print("6: Inverted Triangle")
+    for i in range(1, size+1):
+        for j in range(1, i):
+            print(end=" ")
+        for j in range(i, size+1):
+            print("*", end=" ")
+        print()
+
+    print()
+    print("7: Half Diamond Pattern")
+    n = 5
+    size = n // 2 + 1
+    for i in range(1, size+1):
+        for j in range(1, i+1):
+            print("*", end=" ")
+        print()
+    size -= 1
+    for i in range(1, size+1):
+        for j in range(i, size+1):
+            print("*", end=" ")
+        print()
+    
+loop5to8()
