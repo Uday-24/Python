07a9c1a18dd09f395c0e16138a2f23b3c2a2d214
Uday-24 <chotaliyauday24@gmail.com>
2024-06-08 06:28:24 +0530

    New Pyramid added

--- Pyramids/OthersPyramids.py
+++ Pyramids/OthersPyramids.py
@@ -403,3 +403,21 @@ def loop22():
         print()
 
 loop22()
+
+
+# Floyd's Triangle
+# 1
+# 2 3
+# 4 5 6
+# 7 8 9 10
+def loop23():
+    print("Floyd's Triangle")
+    size = 4
+    k = 1
+    for i in range(1, size+1):
+        for j in range(1, i+1):
+            print(k, end=" ")
+            k+=1
+        print()
+
+loop23()
