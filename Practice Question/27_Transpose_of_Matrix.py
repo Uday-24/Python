e32c8f143d2971b045a56aaba7859b0fbffc36c3
Uday-24 <chotaliyauday24@gmail.com>
2024-06-19 06:31:49 +0530

    Transpose of matrix

--- Practice Question/27_Transpose_of_Matrix.py
+++ Practice Question/27_Transpose_of_Matrix.py
@@ -1,17 +1,36 @@
+# Print the Transpose of matrix
+
+
+# Method 1 : Aam Zindagi
+
+# A = [
+#     [1, 2, 3],
+#     [4, 5, 6]
+# ]
+
+# T = [
+#     [0, 0],
+#     [0, 0],
+#     [0, 0]
+# ]
+
+# for i in range(len(A)):
+#     for j in range(len(A[i])):
+#         T[j][i] = A[i][j]
+    
+# for t in T:
+#     print(t)
+
+
+# Method 2 : Mentos Zindagi
+# Using List Comprehension
+
 A = [
     [1, 2, 3],
     [4, 5, 6]
 ]
 
-T = [
-    [0, 0],
-    [0, 0],
-    [0, 0]
-]
+T = [[A[j][i] for j in range(len(A))] for i in range(len(A[0]))]
 
-for i in range(len(A)):
-    for j in range(len(A[i])):
-        T[j][i] = A[i][j]
-    
-for t in T:
-    print(t)
+for i in T:
+    print(i)
