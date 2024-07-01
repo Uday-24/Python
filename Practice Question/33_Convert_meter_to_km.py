75a942d6df2f2517bf2e7c48d4a62d5e0a3e9465
Uday-24 <chotaliyauday24@gmail.com>
2024-07-01 05:45:32 +0530

    Convert meter to km and viceversa

---
+++ Practice Question/33_Convert_meter_to_km.py
@@ -0,0 +1,9 @@
+# Convert meter to Kilometer and viceversa
+
+meter = float(input("Enter Meters : "))
+km = meter / 1000
+print(f"{meter} m = {km} km")
+
+km = float(input("Enter Kilo Meters : "))
+meter = km * 1000
+print(f"{km} km = {meter} m")
